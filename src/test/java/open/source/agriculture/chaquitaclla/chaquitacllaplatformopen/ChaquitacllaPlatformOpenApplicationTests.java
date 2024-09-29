94b904f8ede9ec5c914f3cf39fd944142019e0fb
PaoloGuillenLuna <129306198+GrandProgTest@users.noreply.github.com>
2024-09-29 14:24:43 -0500

    Update ChaquitacllaPlatformOpenApplicationTests.java

--- src/test/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/ChaquitacllaPlatformOpenApplicationTests.java
+++ src/test/java/open/source/agriculture/chaquitaclla/chaquitacllaplatformopen/ChaquitacllaPlatformOpenApplicationTests.java
@@ -19,7 +19,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
-class SowingCommandServiceImplTest {
+class ChaquitacllaPlatformOpenApplicationTests {
 
     @Mock
     private SowingRepository sowingRepository;
